460374e38c4ec4fc4a725c3979ccd00152881ce3
Jeongyeon Yang <jeong6kite@gmail.com>
2023-03-03 13:45:56 +0900

    그냥 함수 불러서 값 업데이트하기

---
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by 양정연 on 2023/03/03.
+//  Copyright © 2023 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+// ViewModel : View를 표현하기 위한 모델
+struct Menu {
+    var name: String
+    var price: Int
+    var count: Int
+}
---
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  MenuListViewModel.swift
+//  RxSwift+MVVM
+//
+//  Created by 양정연 on 2023/03/03.
+//  Copyright © 2023 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+class MenuListViewModel {
+    
+    var menus: [Menu] = [
+        Menu(name: "튀김1", price: 100, count: 0),
+        Menu(name: "튀김2", price: 300, count: 0),
+        Menu(name: "튀김3", price: 500, count: 0),
+        Menu(name: "튀김4", price: 200, count: 0),
+        Menu(name: "튀김5", price: 300, count: 0)
+    ]
+    
+    var itemsCount: Int = 3
+    var totalPrice: Int = 1_000
+}
