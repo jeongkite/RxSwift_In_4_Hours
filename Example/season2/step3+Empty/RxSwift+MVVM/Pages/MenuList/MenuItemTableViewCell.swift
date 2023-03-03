0606b2dc41ce14899ea470087ff88e808102731b
Jeongyeon Yang <jeong6kite@gmail.com>
2023-03-03 16:17:55 +0900

    +, - 버튼을 구현하는 방법 1. ViewModel에서 업데이트 함수 구현

--- Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuItemTableViewCell.swift
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuItemTableViewCell.swift
@@ -12,8 +12,11 @@ class MenuItemTableViewCell: UITableViewCell {
     @IBOutlet var title: UILabel!
     @IBOutlet var count: UILabel!
     @IBOutlet var price: UILabel!
+    
+    var viewModel: MenuListViewModel!
 
     @IBAction func onIncreaseCount() {
+        viewModel.increaseCount(self)   // 이런식으로 데이터를 넘겨서 업데이트 하는 방법도 있을 것.
     }
 
     @IBAction func onDecreaseCount() {
